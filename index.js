69b1a42c0fabfed8182ca2a5c5c7dcd437708a2e
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-04-13 16:47:11 +0530

    updated

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/profile', (req, res) => {
+  res.send('Hello from profile!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
