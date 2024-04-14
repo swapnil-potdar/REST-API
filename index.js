ef0f817a271f983bf5604d10d1821d7bbd6e273f
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-04-14 17:03:17 +0530

    experimenting with ejs

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.set("view engine", "ejs");
+
+app.use(function(req,res,next){
+  console.log('middleware working');
+  next();
+});
+
+/* app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+}) */
 
 app.get('/profile', (req, res) => {
   res.send('Hello from profile!')
 })
+
+app.get('/profile/:username', (req, res) => {
+  res.send(`Hello from ${req.params.username}`)
+})
+
+app.get('/', (req, res) => {
+  res.render("index",{name:"swap"})
+})
+
+app.get('/contact', (req, res) => {
+  res.render("contact")
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
